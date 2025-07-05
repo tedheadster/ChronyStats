388e2b0d06161a1399507277878ff5bf66109a4b
My Self <whiteheadm@acm.org>
2025-07-05 12:35:49 +0000

    Initial commit

---
+++ ChronyStats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for HOST in $( chronyc -n sources | egrep -v "MS|===|0ns$" | awk '{ print $2 }' )
+do
+   AVG=$( grep ${HOST} ../statistics.log | tail -100 | awk '{ sum += $5 } END { if (sum < 0) { sum = -sum } ; print sum / NR }' )
+   NAME=$( getent hosts ${HOST} | awk '{ print $2 }' )
+   echo ${AVG} ${NAME} ${HOST}
+done | sort -k 1,1g > mystatistics.sort
+
+logrotate --force --verbose ./logrotate.conf
