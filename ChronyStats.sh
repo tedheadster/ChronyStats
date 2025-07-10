5357e1f272f58093fd01d267282ec7616c33cdcc
My Self <whiteheadm@acm.org>
2025-07-10 14:30:34 +0000

    Set number of samples to a variable

--- ChronyStats.sh
+++ ChronyStats.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SAMPLES=1000
+
 for HOST in $( chronyc -n sources | egrep -v "nan|MS|===|0ns$" | awk '{ print $2 }' )
 do
-   AVG=$( grep -a ${HOST} ../statistics.log | tail -100 | awk '{ sum += $5 } END { if (sum < 0) { sum = -sum } ; print sum / NR }' )
+   AVG=$( grep -a ${HOST} ../statistics.log | tail -${SAMPLES} | awk '{ sum += $5 } END { if (sum < 0) { sum = -sum } ; print sum / NR }' )
    NAME=$( getent hosts ${HOST} | awk '{ print $2 }' )
    echo ${AVG} ${NAME} ${HOST}
 done | sort -k 1,1g > mystatistics.sort
